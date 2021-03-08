734bd39c3072cf5e6d7b6dc1ddce294896ca21c7
maxinth <tunde.j.adeleye@gmail.com>
2021-03-08 14:36:39 +0100

    Navbar - initial setup

---
+++ components/HomePageComponents/NavBar/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+const NavContainer = styled.section`
+  color: red;
+`;
+const Navbar = () => {
+  return <NavContainer>nav goes here</NavContainer>;
+};
+
+export default Navbar;
---
+++ components/HomePageComponents/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "./NavBar";
+import styled from "styled-components";
+
+const Container = styled.section``;
+const Home = () => {
+  return (
+    <Container>
+      <NavBar />
+    </Container>
+  );
+};
+
+export default Home;
