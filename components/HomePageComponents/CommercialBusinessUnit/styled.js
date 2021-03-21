5cd224b249ce1a58cc915d33ed6f1dc21a13bd88
maxinth <tunde.j.adeleye@gmail.com>
2021-03-21 12:54:29 +0100

    added CommercialbusinessUnit

--- components/HomePageComponents/CommercialBusinessUnit/styled.js
+++ components/HomePageComponents/CommercialBusinessUnit/styled.js
@@ -9,19 +9,26 @@ const Container = styled.section`
 const Title = styled.h1`
   text-align: center;
   width: 100%;
+  transition: font-size 0.2;
+  @media (min-width: 900px) {
+    font-size: 2rem;
+  }
 `;
 
 const CardImage = styled.img`
   display: block;
   width: 100%;
   height: 100%;
-  min-height: 40px;
+
+  /* min-height: 40px; */
   object-fit: contain;
+  transition: opacity 0.2;
 `;
 
 const CardTitle = styled.span`
   margin: 1rem 0;
   text-transform: capitalize;
+  text-align: center;
 `;
 const CardBox = styled.div`
   display: flex;
@@ -32,9 +39,31 @@ const CardBox = styled.div`
   margin-bottom: 1rem;
   box-shadow: 1px 2px 4px grey;
   border-radius: 5px;
+  transition: all 0.2s;
+  cursor: pointer;
+  > * {
+    flex: 1;
+  }
+  ${CardImage} {
+    opacity: 0.7;
+  }
+  max-width: 400px;
+  :hover {
+    transform: scale(1.02);
+    background-color: rgb(243, 192, 66);
+    ${CardImage} {
+      opacity: 1;
+    }
+  }
 
-  @media (min-width: 481px) {
-    max-width: 300px;
+  /* max-width: 300px; */
+
+  @media (min-width: 531px) {
+    /* max-width: 350px; */
+  }
+  @media (min-width: 674px) {
+    margin-bottom: unset;
+    /* max-width: 350px; */
   }
 `;
 
@@ -42,5 +71,21 @@ const CardsContainer = styled.section`
   padding: 2rem;
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  @media (min-width: 625px) {
+    display: grid;
+    gap: 20px;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  }
+  @media (min-width: 674px) {
+    display: grid;
+    gap: 20px;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  }
+  /* @media (min-width: 700px) {
+    grid-template-columns: repeat(auto-fit, minmax(800px, 1fr));
+  } */
 `;
 export { Container, Title, CardBox, CardImage, CardTitle, CardsContainer };
