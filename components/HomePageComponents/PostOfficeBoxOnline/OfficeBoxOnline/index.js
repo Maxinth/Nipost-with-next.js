1e380a1064bfeabebf4004625b93fce71c1f4dea
maxinth <tunde.j.adeleye@gmail.com>
2021-03-23 10:09:52 +0100

    Steps - first go

--- components/HomePageComponents/PostOfficeBoxOnline/OfficeBoxOnline/index.js
+++ components/HomePageComponents/PostOfficeBoxOnline/OfficeBoxOnline/index.js
@@ -1,9 +1,19 @@
 import React from "react";
-import { OfficeSection, Heading } from "./styled";
+import { OfficeSection, Heading, Text } from "./styled";
+import Steps from "../Steps";
 const OfficeBoxOnline = () => {
   return (
     <OfficeSection>
       <Heading>POST OFFICE BOX ONLINE</Heading>
+      <Text>
+        Getting your post office box is a lot easy now. Your can now complete
+        your application online and your are assigned a post office box after
+        completion of the application process.
+      </Text>
+      <Heading as="h5" sm>
+        STEPS FOR POST OFFICE BOX ONLINE APPLICATION
+      </Heading>
+      <Steps />
     </OfficeSection>
   );
 };
--- components/HomePageComponents/PostOfficeBoxOnline/OfficeBoxOnline/styled.js
+++ components/HomePageComponents/PostOfficeBoxOnline/OfficeBoxOnline/styled.js
@@ -1,10 +1,26 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const OfficeSection = styled.section`
   font-family: "Open Sans", sans-serif;
+  padding: 1rem 2rem;
 `;
 
 const Heading = styled.h4`
-  font-size: 2rem;
+  font-size: 1.8rem;
+  color: #333;
+  text-align: center;
+  margin: 1rem 0;
+  font-weight: 500;
+
+  ${({ sm }) =>
+    sm &&
+    css`
+      font-size: 1rem;
+    `}
 `;
-export { OfficeSection, Heading };
+
+const Text = styled.p`
+  font-size: 1.1rem;
+  line-height: 1.7;
+`;
+export { OfficeSection, Heading, Text };
