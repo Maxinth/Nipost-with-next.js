84b0a87e3303b30b359e6ba39ce21681d89014da
maxinth <tunde.j.adeleye@gmail.com>
2021-03-24 13:02:42 +0100

    InPageNav - first go

--- components/CalculatorPageComps/index.js
+++ components/CalculatorPageComps/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Banner from "../SharedComponents/Banner";
-
+import { Container } from "./styled";
 const CalculatorPage = () => {
   return (
-    <>
+    <Container>
       <Banner text="Online Calculator" />
-    </>
+    </Container>
   );
 };
 
---
+++ components/CalculatorPageComps/styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const Container = styled.section`
+  background-color: rgb(254, 254, 254);
+`;
+
+export { Container };
